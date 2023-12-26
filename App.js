09e514ca187438d21f97e1295f4bc0a4ef47a12f
Nicholas Veras <nicholasveras@MacBook-Air-de-Nicholas.local>
2023-12-26 17:28:50 -0300

    praticando useEffect

--- App.js
+++ App.js
@@ -1,11 +1,27 @@
+import React, {useState, useEffect} from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
+  const [contador, setContador] = useState(0);
+  const [renderizado, setRenderizado] = useState(true);
+
+  
+  useEffect(() => {
+    console.log('MONTOU!')
+  },[]);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Button title='Aumentar' onPress={() => setContador(contador+1) }/>
+      <Text style={{fontSize: 30}}>{contador}</Text>
+      <Button title='Diminuir' onPress={() => setContador(contador-1) }/>
       <StatusBar style="auto" />
+
+      <Button title='MOSTRAR NOME' onPress={() => setRenderizado(false)}/>
+
+      {renderizado && <Nome/>}
+
     </View>
   );
 }
@@ -18,3 +34,15 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+function Nome(){
+
+  useEffect(() => {
+    console.log('COMPONENTE NOME FOI MONTADO NA TELA!')
+
+    return () => console.log('COMPONENTE DESMONTADO!')
+
+  }, []);
+
+  return <Text>NICHOLASSSSS</Text>
+}
